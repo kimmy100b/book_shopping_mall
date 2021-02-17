3b48d66259e5638f0fd4f15020b895cd56c6b893
DESKTOP-J5N5A82\LG <kimmy100b@gmail.com>
2021-02-18 00:44:39 +0900

    회원가입 아이디 중복체크 안 했을 때 alert창, 데이터 없을 때 alert창

--- src/main/java/com/bookshop01/member/dao/MemberDAO.java
+++ src/main/java/com/bookshop01/member/dao/MemberDAO.java
@@ -8,6 +8,8 @@ import com.bookshop01.member.vo.MemberVO;
 
 public interface MemberDAO {
 	public MemberVO login(Map loginMap) throws DataAccessException;
+
 	public void insertNewMember(MemberVO memberVO) throws DataAccessException;
+
 	public String selectOverlappedID(String id) throws DataAccessException;
 }
